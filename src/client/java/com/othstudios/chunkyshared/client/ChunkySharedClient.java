f68621615df1323d51f2df7ecf7561b1d6d2eb6d
Stephen Kiser <stephen.kiser@onthehillstudios.com>
2026-08-02 18:09:17 -0400

    Initial commit: Fabric mod scaffold

---
+++ src/client/java/com/othstudios/chunkyshared/client/ChunkySharedClient.java
@@ -0,0 +1,10 @@
+package com.othstudios.chunkyshared.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChunkySharedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
